78110406c86e238a00af85f2a522bec3b694b2b5
azinisikhona <azizsiyalana10@gmail.com>
2024-02-19 12:04:28 +0200

    fixed images

--- src/js/products.js
+++ src/js/products.js
@@ -1,7 +1,7 @@
 const Products = [
   {
       id: 1,
-      image: "https://i.postimg.cc/26zrdvPQ/her-her-deluxe-edition.webp",
+      image: "https://i.postimg.cc/sX1yYhXv/her-her-deluxe-edition.webp",
       name: "H.E.R. - H.E.R. (Deluxe Edition",
       desc: "H.E.R. by anonymous singer-songwriter H.E.R. (Gabi Wilson), is a collection of her previous releases Vol. 1 and Vol. 2. This deluxe edition also include six previously unreleased tracks to create a full-length release.",
       price: 559.60,
@@ -9,7 +9,7 @@ const Products = [
   },
   {
       id: 2,
-      image: "https://i.postimg.cc/fb4Dq10f/dave-chappelle.webp",
+      image: "https://i.postimg.cc/x1YwG4TX/product-namecoast-to-coast-collectibles-memorabilia-148588.jpg",
       name: "Dave Chappelle",
       desc: "A signed LP Cover. Includes the vinyl. '846'. Guaranteed authentic. This was signed in-person by Dave Chappelle. Guaranteed authentic. Includes a certificate of authenticity (COA) provided by JSA. The certification number is: AB38903",
       price: 12569.31,
@@ -17,7 +17,7 @@ const Products = [
   },
   {
       id: 3,
-      image: "https://i.postimg.cc/NfF5TqcN/Marvi-Gaye.webp",
+      image: "https://i.postimg.cc/SQ1Lf2SC/97777-large.jpg",
       name: "Marvin Gaye - What's going on",
       desc: "This is a 12”Vinyl Record with a Marvin Gaye What's Going On Reproduction Label added and has the Autograph of Marvin Gaye printed directly on to it using a Unique State of the Art printing system",
       price: 22345.44,
@@ -25,7 +25,7 @@ const Products = [
   },
   {
       id: 4,
-      image: "https://i.postimg.cc/jd0z4SM8/2Pac.webp",
+      image: "https://i.postimg.cc/tJRNBWwG/61-Pn-BPq-Tmx-L-UF1000-1000-QL80.jpg",
       name: "2Pac",
       desc: "'Greatest Hits' is a posthumous greatest hits album by the American rapper. The album's non-chronological sequence focuses on the highlights of the rappers career. The album is comprised of 21 tracks, accompanied by four previously unreleased songs. Features the singles 'Changes' and 'Unconditional Love'.",
       price: 3200.00,
@@ -33,7 +33,7 @@ const Products = [
   },
   {
       id: 5,
-      image: "https://i.postimg.cc/zvr26ZCK/Selena-gomez.webp",
+      image: "https://i.postimg.cc/wMtQKgqw/ZELW-o-Wv-BQa-Su-e-IBu43-Tt-EAi6-JOLZ4wa-AHCCRbz-Dhk.webp",
       name: "Selena Gomez LP Album",
       desc: "This Selena Gomez autographed LP Album Vinyl Insert has been personally hand-signed by Selena Gomez.",
       price: 1863.58,
@@ -41,7 +41,7 @@ const Products = [
   },
   {
       id: 6,
-      image: "https://i.postimg.cc/PfCF3y1S/Nora.webp",
+      image: "https://i.postimg.cc/QdrNWmTt/1200x1200bf-60.jpg",
       name: "Nora Noor - Soul Deep",
       desc: "Blue Mood is proud to present the comeback album from the Norwegian Queen of Soul. Noora Noor is finally back. Soul Deep, that gave Noor the Norwegian Grammy Award (Spellemannprisen), goes more retro and was recorded live in studio. ",
       price: 420.00,
@@ -49,7 +49,7 @@ const Products = [
   },
   {
       id: 7,
-      image: "https://i.postimg.cc/hG0m7ndZ/jessica-simpson.webp",
+      image: "https://i.postimg.cc/Twq3RH0g/main-1644332854-Jessica-Simpson-Signed-Irresistible-Vinyl-Record-Album-Cover-JSA-Beckett-Pristine-Auc.jpg",
       name: "Jessica Simpson - Irresistible",
       desc: "A copy of 'Irresistible' signed by Jessica. Autographed in-person LP album record cover. Includes the vinyl. Guaranteed authentic.",
       price: 7415.83,
@@ -57,7 +57,7 @@ const Products = [
   },
   {
       id: 8,
-      image: "https://i.postimg.cc/cChfySs8/kanye-west.webp",
+      image: "https://i.postimg.cc/9fcLzDtS/s-l1200.webp",
       name: "Kanye West - Ye",
       desc: "A signed LP Cover. Includes the vinyl. 'Ye''. This was signed in-person by Kanye West. Guaranteed authentic. Includes a certificate of authenticity (COA) provided by GA. The certification number is: GV939134",
       price: 14813.72,
@@ -65,7 +65,7 @@ const Products = [
   },
   {
       id: 9,
-      image: "https://i.postimg.cc/xdNrcpm7/tina-turner.webp",
+      image: "https://i.postimg.cc/Hx8mvvcb/190295176532.avif",
       name: "Tina Turner - Foreign affair",
       desc: "",
       price: 12566.98,
